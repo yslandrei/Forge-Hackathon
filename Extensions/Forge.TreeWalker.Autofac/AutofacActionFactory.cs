0e1e4820a020579d24fef4b38a41645799868da9
Andrei Diaconescu <andreimihaid@microsoft.com>
2025-09-22 13:15:52 +0300

    Autofac di support

---
+++ Extensions/Forge.TreeWalker.Autofac/AutofacActionFactory.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using System;
+
+namespace Microsoft.Forge.TreeWalker.Autofac
+{
+    public class AutofacActionFactory : IForgeActionFactory
+    {
+        private readonly IComponentContext _container;
+
+        public AutofacActionFactory(IComponentContext container)
+        {
+            _container = container;
+        }
+
+        public BaseAction CreateInstance(Type type)
+        {
+            return _container.Resolve(type) as BaseAction;
+        }
+
+        public BaseAction CreateInstance(Type type, TreeWalkerParameters parameters)
+        {
+            return _container.Resolve(type, new NamedParameter("parameters", parameters)) as BaseAction;
+        }
+    }
+}
---
+++ Extensions/Forge.TreeWalker.Autofac/AutofacExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using System.Reflection;
+
+namespace Microsoft.Forge.TreeWalker.Autofac
+{
+    public static class AutofacExtensions
+    {
+        public static ContainerBuilder RegisterForgeActionsFromAssembly(this ContainerBuilder builder, Assembly assembly)
+        {
+            builder.RegisterAssemblyTypes(assembly)
+                   .Where(t => t.GetCustomAttributes(typeof(Attributes.ForgeActionAttribute), true).Length > 0)
+                   .AsSelf()
+                   .InstancePerDependency();
+
+            return builder;
+        }
+
+        public static ContainerBuilder RegisterForge(this ContainerBuilder builder)
+        {
+            builder.RegisterType<SubroutineAction>().AsSelf();
+
+            builder.RegisterType<AutofacActionFactory>()
+                   .As<IForgeActionFactory>()
+                   .SingleInstance();
+
+            builder.RegisterType<AutofacTreeWalkerSessionFactory>()
+                   .As<ITreeWalkerSessionFactory>()
+                   .SingleInstance();
+
+            return builder;
+        }
+    }
+}
