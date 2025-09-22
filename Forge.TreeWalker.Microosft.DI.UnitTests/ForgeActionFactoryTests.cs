381b9c07c0623e41b2e3cafded0b4dff38306a2a
Dušan Bobičić <duanbobii@microsoft.com>
2025-09-22 18:37:15 +0200

    Add ForgeActionFactory UTs

---
+++ Forge.TreeWalker.Microosft.DI.UnitTests/ForgeActionFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Forge.TreeWalker.Microosft.DI.UnitTests.Mocks;
+using Forge.TreeWalker.Microsoft.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Forge.TreeWalker;
+using System.Reflection;
+
+namespace Forge.TreeWalker.Microosft.DI.UnitTests
+{
+    [TestClass]
+    public sealed class ForgeActionFactoryTests
+
+    {
+        [TestMethod]
+        public void ShouldResolveForgeActionFactory()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.RegisterForgeActionsFromAssembly(Assembly.GetExecutingAssembly());
+            serviceCollection.RegisterForge();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var factory = serviceProvider.GetService<IForgeActionFactory>();
+
+            Assert.IsNotNull(factory);
+        }
+
+        [TestMethod]
+        public void FactoryShouldCreateActionInstance()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.RegisterForgeActionsFromAssembly(Assembly.GetExecutingAssembly());
+            serviceCollection.RegisterForge();
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var factory = serviceProvider.GetService<IForgeActionFactory>();
+            var testAction = factory.CreateInstance(typeof(TestForgeAction));
+
+            Assert.IsNotNull(testAction);
+        }
+    }
+}
---
+++ Forge.TreeWalker.Microosft.DI.UnitTests/Mocks/TestForgeAction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Forge.Attributes;
+using Microsoft.Forge.TreeWalker;
+
+namespace Forge.TreeWalker.Microosft.DI.UnitTests.Mocks
+{
+
+    [ForgeAction(typeof(TestForgeActionInput))]
+    internal class TestForgeAction : BaseAction
+    {
+        public override Task<ActionResponse> RunAction(ActionContext actionContext)
+        {
+            return Task.FromResult(new ActionResponse());
+        }
+    }
+
+    internal class TestForgeActionInput { }
+}
